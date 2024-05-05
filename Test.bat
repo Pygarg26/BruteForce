1ce15ac58f8624a2ffbe77deaa9c333b58fbe74e
Pygarg26 <167622704+Pygarg26@users.noreply.github.com>
2024-05-05 13:33:09 +0200

    Update Test.bat

--- Test.bat
+++ Test.bat
@@ -1,15 +1,29 @@
 @echo off
-title SMB Bruteforce - by Ebola Man
-color A
+title Window Server Brute-Force by Pygarg26
+color 02
 echo.
 set /p ip="Enter IP Address: "
 set /p user="Enter Username: "
-set /p wordlist="Enter Password List: "
+set maj = A Z E R T Y U I O P Q S D F G H J K L M W X C V B N
+set min = a z e r t y u i o p q s d f g h j k l m w x c v b n
+set numbers = 1 2 3 4 5 6 7 8 9 0
 
 set /a count=1
-for /f %%a in (%wordlist%) do (
-  set pass=%%a
-  call :attempt
+set current = none
+for %%a in (%maj%) do (
+  set current = %%a
+	for %%a in (%min%) do (
+		set current = %current%%%a
+			for %%a in (%min%) do (
+				set current = %current%%%a
+				for %%a in (%min%) do (
+					set current = %current%%%a
+					for %%i in (1 1 1 1) do (
+						fo
+					)
+				)
+			)
+	)
 )
 echo Password not Found :(
 pause
